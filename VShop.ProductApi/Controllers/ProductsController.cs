3214b1fa37e51a181cf3a80a4d6d956ba126ad23
Mouracfs <danielmourajuve10@gmail.com>
2023-08-15 22:20:48 -0300

    Adding Controllers, interfaces and Services

---
+++ VShop.ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Services;
+
+namespace VShop.ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var productsDto = await _productService.GetProducts();
+            if (productsDto is null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(productsDto);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get(int id)
+        {
+            var productDto = await _productService.GetProductById(id);
+            if (productDto is null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(productDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDto)
+        {
+
+            if (productDto == null)
+            {
+                return BadRequest("Data Invalid");
+            }
+            await _productService.AddProduct(productDto);
+
+            return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id },
+                productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDto)
+        {
+
+            if (id != productDto.Id)
+            {
+                return BadRequest("Data Invalid");
+            }
+
+            if (productDto == null)
+            {
+                return BadRequest("Data Invalid");
+            }
+
+            await _productService.UpdateProduct(productDto);
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id)
+        {
+            var productDto = await _productService.GetProductById(id);
+            
+            if (productDto == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            await _productService.RemoveProduct(id);
+            return Ok(productDto);
+        }
+    }
+}
---
+++ VShop.ProductApi/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Services;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<IEnumerable<CategoryDTO>> GetCategoriesProducts();
+    Task<CategoryDTO> GetCategoryById(int id);
+    Task AddCategory(CategoryDTO categoryDto);
+    Task UpdateCategory(CategoryDTO categoryDto);
+    Task RemoveCategory(int id);
+
+}
