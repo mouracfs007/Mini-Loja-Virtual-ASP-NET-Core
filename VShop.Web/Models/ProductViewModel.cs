e2b58f159e7d73312bd1025b322742bcdd7163d1
Mouracfs <danielmourajuve10@gmail.com>
2023-08-17 17:53:47 -0300

    Add Views: Products, implements Interface and Services

--- VShop.Web/Models/ProductViewModel.cs
+++ VShop.Web/Models/ProductViewModel.cs
@@ -5,12 +5,18 @@ namespace VShop.Web.Models;
 
 public class ProductViewModel
 {
-    public int Id { get; set; }   
-    public string? Name { get; set; }   
-    public decimal Price { get; set; } 
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
     public string? Description { get; set; }
+    [Required]
     public long Stock { get; set; }
+    [Required]
     public string? ImageURL { get; set; }
     public string? CategoryName { get; set; }
+    [Display(Name = "Categories")]
     public int? CategoryId { get; set; }
 }
