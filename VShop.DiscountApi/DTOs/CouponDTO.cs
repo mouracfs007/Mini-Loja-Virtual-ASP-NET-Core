c5ddd05b90d879df64c8c53e9e9cb41452456c57
Mouracfs <danielmourajuve10@gmail.com>
2023-08-25 22:37:35 -0300

    Microservice: DiscountApi. Controllers, Models, Migrations and Repositories (DiscountApi)

---
+++ VShop.DiscountApi/DTOs/CouponDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.DiscountApi.DTOs
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        [Required]
+        public string? CouponCode { get; set; }
+        [Required]
+        public decimal Discount { get; set; }
+    }
+}
---
+++ VShop.DiscountApi/Models/Coupon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VShop.DiscountApi.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+
+        [StringLength(30)]
+        public string? CouponCode { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Discount { get; set; }
+    }
+}
