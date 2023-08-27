046ea828fa76d921d675e32a0887c678ba7fc7fe
Mouracfs <danielmourajuve10@gmail.com>
2023-08-27 02:31:10 -0300

    Adding Function JS to view counter in Shopping Cart

--- VShop.Web/wwwroot/js/site.js
+++ VShop.Web/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+// Adicionar
+
+$(document).ready(function () {
+    var storedCount = localStorage.getItem('carrinhoCount');
+    count = storedCount ? parseInt(storedCount) : 0;
+    carrinhoIndex.innerHTML = count;
+});
+
+
+function acrescentar() {
+    count++;
+    carrinhoIndex.innerHTML = count;
+
+    // Armazenar o valor no localStorage
+    localStorage.setItem('carrinhoCount', count);
+}
+
+function remover() {
+    count--;
+    carrinhoIndex.InnerHTML = count;
+
+    localStorage.setItem('carrinhoCount', count);
+}
+
+
